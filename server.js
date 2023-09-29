028fc3529f558a491921ab55692f769bbe1600e9
Raz <amsalemraz10@gmail.com>
2023-09-29 18:45:36 +0300

    Backend has been converted to use MongoDB and support user registration and login

--- server.js
+++ server.js
@@ -1,128 +1,55 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express'
-import cors from 'cors'
+import express  from 'express'
 import cookieParser from 'cookie-parser'
+import cors  from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-import { toyService } from './services/toy.service.js'
-import { loggerService } from './services/logger.service.js'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { logger } from './services/logger.service.js'
+logger.info('server.js loaded...')
+
 const app = express()
 
-// App Configuration
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:8080',
-        'http://localhost:8080',
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-    ],
-    credentials: true
-}
-
-app.use(cors(corsOptions))
-app.use(cookieParser()) // for res.cookies
-app.use(express.json()) // for req.body
+// Express App Config
+app.use(cookieParser())
+app.use(express.json())
 app.use(express.static('public'))
 
-
-
-//List
-app.get('/api/toy', (req, res) => {
-    const { txt, labels, inStock } = req.query
-
-    const filterBy = { txt, labels, inStock }
-    toyService.query(filterBy)
-        .then(toys => {
-            res.send(toys)
-        })
-        .catch(err => {
-            loggerService.error('Cannot load toys', err)
-            res.status(400).send('Cannot load toys')
-        })
-})
-
-//add
-app.post('/api/toy', (req, res) => {
-    const { name, price, labels, inStock } = req.body
-    console.log('test', req.body)
-
-    const toy = {
-        name,
-        price,
-        labels,
-        inStock,
+if (process.env.NODE_ENV === 'production') {
+    // Express serve static files on production environment
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    console.log('__dirname: ', __dirname)
+} else {
+    // Configuring CORS
+    const corsOptions = {
+        // Make sure origin contains the url your frontend is running on
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173','http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
     }
+    app.use(cors(corsOptions))
+}
 
-    toyService.save(toy)
-        .then(savedToy => {
-            res.send(savedToy)
-        })
-        .catch(err => {
-            loggerService.error('Cannot add toy', err)
-            res.status(400).send('Cannot add toy')
-        })
-})
-///edit
-app.put('/api/toy', (req, res) => {
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { toyRoutes } from './api/toy/toy.routes.js'
 
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/toy', toyRoutes)
 
-    const { _id, name, labels, price, createdAt, inStock } = req.body
-    const toy = {
-        _id,
-        name,
-        labels,
-        price,
-        createdAt,
-        inStock
-    }
-    toyService.save(toy)
-        .then(savedToy => {
-            res.send(savedToy)
-        })
-        .catch(err => {
-            loggerService.error('Cannot add toy', err)
-            res.status(400).send('Cannot add toy')
-        })
-})
-
-// Read - getById
-app.get('/api/toy/:toyId', (req, res) => {
-    const { toyId } = req.params
-    toyService.get(toyId)
-        .then(toy => {
-            toy.msgs = ['Dummy msg from Backend...']
-            console.log(toy);
-            res.send(toy)
-        })
-        .catch(err => {
-            loggerService.error('Cannot get toy', err)
-            res.status(400).send(err)
-        })
-})
-
-// Remove
-app.delete('/api/toy/:toyId', (req, res) => {
-    const { toyId } = req.params
-    toyService.remove(toyId)
-        .then(msg => {
-            res.send({ msg, toyId })
-        })
-        .catch(err => {
-            loggerService.error('Cannot delete toy', err)
-            res.status(400).send('Cannot delete toy, ' + err)
-        })
-})
+// Make every unmatched server-side-route fall back to index.html
+// So when requesting http://localhost:3030/index.html/car/123 it will still respond with
+// our SPA (single page app) (the index.html file) and allow vue-router to take it from there
 
 app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.resolve('public/index.html'))
 })
 
-
-
-// Listen will always be the last line in our server!
 const port = process.env.PORT || 3030
+
 app.listen(port, () => {
-    loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
+    logger.info('Server is running on port: ' + port)
 })
